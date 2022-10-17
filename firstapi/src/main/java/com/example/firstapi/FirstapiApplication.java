1212c78142530bbe6b12b3ffdd0846f1a04f3b65
maeddes <maeddes@gmail.com>
2022-10-17 14:40:26 +0000

    some more stuff

--- firstapi/src/main/java/com/example/firstapi/FirstapiApplication.java
+++ firstapi/src/main/java/com/example/firstapi/FirstapiApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstapiApplication {
 
 	String property = "unset";
+	String newprop = "";
 
 	@RequestMapping(method = RequestMethod.GET, path = "/")
 	public String sayHello(){
@@ -25,8 +26,14 @@ public class FirstapiApplication {
 	public String postVariable(@PathVariable String variable){
 
 		property = variable;
-
 		return property;
+		
+	}
+
+	@GetMapping("fakeGet/{parameter}")
+	public String fakeGet(@PathVariable String parameter){
+
+		return "Parameter given was: "+parameter;
 	}
 
 	@GetMapping("/test")
