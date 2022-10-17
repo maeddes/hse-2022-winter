0e0f1a2fa78804e7ea228ec7b407894cc0925123
Matthias Haeussler <maeddes@gmail.com>
2022-10-17 15:59:51 +0200

    added Spring Boot first app skeleton

---
+++ firstapi/src/main/java/com/example/firstapi/FirstapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstapiApplication.class, args);
+	}
+
+}
---
+++ firstapi/src/test/java/com/example/firstapi/FirstapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
