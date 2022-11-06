77b4b20a37ac2b61e0b7d464512148a4dc0b81cb
Matthias Haeussler <maeddes@gmail.com>
2022-11-06 14:28:56 +0100

    delete and get method

--- firstapi/src/main/java/com/example/firstapi/ApiController.java
+++ firstapi/src/main/java/com/example/firstapi/ApiController.java
@@ -1,10 +1,12 @@
 package com.example.firstapi;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,16 +24,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 // CRUD Controller class
 @RestController
-@RequestMapping("/api/v2/items")
+@RequestMapping("/todos")
 public class ApiController {
 
     private ArrayList<TodoItem> items = new ArrayList<TodoItem>();
-
-    @GetMapping("/version")
-    public String getVersion(){
-
-        return "v2";
-    }
 
     // Add new item to list
     // version 1: using path variables
@@ -42,7 +38,7 @@ public class ApiController {
     })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{name}")
-    public TodoItem createAndAddItem(@PathVariable String name){
+    public TodoItem createAndAddTodoItem(@PathVariable String name){
 
         TodoItem item = new TodoItem(name);
         items.add(item);
@@ -61,7 +57,7 @@ public class ApiController {
     })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/")
-    public TodoItem addItem(@RequestBody TodoItem item){
+    public TodoItem addTodoItem(@RequestBody TodoItem item){
 
         items.add(item);
         return item;
@@ -75,22 +71,57 @@ public class ApiController {
                         @ApiResponse(responseCode = "200", description = "List all items" , content = @Content)
                     })
     @ResponseStatus(HttpStatus.OK)
-    public List<TodoItem> getItems(){
+    public List<TodoItem> getTodoItems(){
 
         return items;
     }
 
-    @Operation(summary = "Find a shopping item by its itemId")
+    @Operation(summary = "Find a ToDo item by its itemId")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found the item", content = {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = TodoItem.class)) }),
             @ApiResponse(responseCode = "400", description = "Invalid itemId supplied", content = @Content),
             @ApiResponse(responseCode = "404", description = "Item not found", content = @Content) })
     @GetMapping(produces = "application/json", path = "/{itemId}")
-    Optional<TodoItem> getShoppingItem(@PathVariable long itemId) {
+    Optional<TodoItem> getTodoItem(@PathVariable String itemId) {
+
+        TodoItem tempItem = new TodoItem(itemId);
+        Optional<TodoItem> returnItem = Optional.empty();
+
+        for(TodoItem item : items){
+
+            if (item.equals(tempItem)) returnItem = Optional.of(item);
+
+        }
+
+        return returnItem;
+
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(produces = "application/json", path = "/{itemId}")
+    TodoItem deleteTodoItem(@PathVariable String itemId){
+
+        TodoItem tempItem = new TodoItem(itemId);
+        Iterator<TodoItem> iterator = items.iterator();
+
+        while(iterator.hasNext()){
+
+            if(iterator.next().equals(tempItem))
+                iterator.remove();
+
+        }
 
         return null;
+    }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(consumes = "application/json", produces = "application/json", path = "/")
+    TodoItem deleteTodo(@RequestBody TodoItem item){
+
+        items.remove(item);
+
+        return null;
     }
 
     // Update an item in the list
