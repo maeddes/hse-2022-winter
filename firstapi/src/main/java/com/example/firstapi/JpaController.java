6b99be8451194258ae42c57e1e64d9cf2e588aef
maeddes <maeddes@gmail.com>
2022-12-29 09:08:09 +0000

    CORS

--- firstapi/src/main/java/com/example/firstapi/JpaController.java
+++ firstapi/src/main/java/com/example/firstapi/JpaController.java
@@ -20,6 +20,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/todos")
 public class JpaController {
 
