33115f5d234ebce5c6118293b3c61a8bc74a34c3
maeddes <maeddes@gmail.com>
2023-01-02 10:15:09 +0000

    cors

--- firstapi/src/main/java/com/example/firstapi/JpaController.java
+++ firstapi/src/main/java/com/example/firstapi/JpaController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
