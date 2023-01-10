463e5693cc649cb303cf53492a87c0228848bc28
Matthias Haeussler <maeddes@gmail.com>
2023-01-10 21:38:52 +0000

    Kotlin stuff

---
+++ springdatarest/src/main/kotlin/de/maeddes/SpringDataRest/DemoApplication.kt
@@ -0,0 +1,29 @@
+package de.maeddes.springdatarest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.repository.CrudRepository
+import org.springframework.web.bind.annotation.RestController
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.web.bind.annotation.RequestMapping
+
+@SpringBootApplication
+@RestController
+class DemoApplication ()
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DemoApplication::class.java, *args)
+}
+
+@Entity
+class TodoItem(
+        @Id
+        var todo: String,
+		val priority: Int)
+
+@RepositoryRestResource(collectionResourceRel = "todos", path = "todos")
+interface TodoItemRepository : CrudRepository<TodoItem, String> {}
---
+++ springdatarest/src/test/kotlin/de/maeddes/SpringDataRest/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.maeddes.SpringDataRest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
